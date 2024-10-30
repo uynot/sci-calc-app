b7412b86b951d971304a433ab2304adfb6d81c0a
uy <kaho399@gmail.com>
2024-10-30 01:15:31 -0400

    completed design, added clicking sound

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				mono: ["Roboto Mono", "monospace"],
+			},
+			fontSize: {
+				xs: "0.65rem",
+				sm: "0.75rem",
+				base: "0.875rem",
+				lg: "1rem",
+				xl: "1.125rem",
+				"2xl": "1.25rem",
+			},
+		},
 	},
 	plugins: [],
 };
